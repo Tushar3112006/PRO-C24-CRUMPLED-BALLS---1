abc86d1642d4d6e67f829924dfb9ac7efd46f00a
Tushar3112006 <72294801+Tushar3112006@users.noreply.github.com>
2021-02-12 21:44:53 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,18 @@
+class Paper{
+    constructor(x,y,radius){
+        var option = {
+            isstatic : false
+
+        }
+        
+        display();
+        fill("yellow")
+        ellipsmode(radius);
+        ellipsmode(this.body,postion.x,this.body.postion.y,)
+
+
+        
+
+        
+    }
+}
